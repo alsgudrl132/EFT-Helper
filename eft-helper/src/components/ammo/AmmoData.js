5047e40a1da44ec0eedd84e6d937dfdbcfecafee
alsgudrl132 <alsgudrl132@gmail.com>
2023-10-01 21:58:16 +0900

    탄약 불러오기

--- eft-helper/src/components/ammo/AmmoData.js
+++ eft-helper/src/components/ammo/AmmoData.js
@@ -4,107 +4,122 @@ import './AmmoData.css';
 
 const AmmoData = () => {
     const [ammoImg, setAmmoImg] = useState('');
+    const [ammo, setAmmo] = useState([]);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('https://api.tarkov.dev/graphql', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Accept: 'application/json',
-                    },
-                    body: JSON.stringify({
-                        query: `{
-                            items(name: "m855a1") {
-                                iconLink
-                            }
-                        }`,
-                    }),
-                });
+                const response = await fetch('http://125.142.47.191:5000/EFT_API/ALL_SELECT');
                 const data = await response.json();
-                setAmmoImg(data.data.items[0].iconLink);
+                setAmmo(data);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
 
         fetchData();
+
+        //     const fetchData = async () => {
+        //         try {
+        //             const response = await fetch('https://api.tarkov.dev/graphql', {
+        //                 method: 'POST',
+        //                 headers: {
+        //                     'Content-Type': 'application/json',
+        //                     Accept: 'application/json',
+        //                 },
+        //                 body: JSON.stringify({
+        //                     query: `{
+        //                         items(name: "m855a1") {
+        //                             iconLink
+        //                         }
+        //                     }`,
+        //                 }),
+        //             });
+        //             const data = await response.json();
+        //             setAmmoImg(data.data.items[0].iconLink);
+        //         } catch (error) {
+        //             console.error('Error fetching data:', error);
+        //         }
+        //     };
+
+        //     fetchData();
     }, []);
     const colors = ['#4CF057', '#87D43D', '#C0B825', '#F99D10', '#EA6B0A', '#DC3B07', '#CF0B04'];
     function getRecoilStyle(recoilValue) {
         if (recoilValue > 0) {
-            return colors[6];
+            return { backgroundColor: colors[6], color: '#f1efef' };
         } else if (recoilValue < 0) {
-            return colors[0];
+            return { backgroundColor: colors[0] };
         }
     }
     function getAccuracyStyle(accuracyValue) {
         if (accuracyValue > 0) {
-            return colors[0];
+            return { backgroundColor: colors[0] };
         } else if (accuracyValue < 0) {
-            return colors[6];
+            return { backgroundColor: colors[6], color: '#f1efef' };
         }
     }
     function getPenetrateStyle(level) {
         level = Math.min(Math.max(level, 1), 6);
+        const color = colors[7 - level];
 
-        return colors[7 - level];
+        return { backgroundColor: color }; // 스타일 객체로 반환
     }
-    // async function logJSONData() {
-    //     const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year');
-    //     const jsonData = await response.json();
-    //     console.log(jsonData);
-    // }
-    // logJSONData();
 
+    console.log(ammo);
     return (
-        <div className="container">
-            <table className="ammo-table">
-                <thead className="ammo-thead">
-                    <tr className="ammo-tr">
-                        <th>탄약 사진</th>
-                        <th>탄약 이름</th>
-                        <th>탄약 데미지</th>
-                        <th>반동</th>
-                        <th>정밀도</th>
-                        <th>클래스1</th>
-                        <th>클래스2</th>
-                        <th>클래스3</th>
-                        <th>클래스4</th>
-                        <th>클래스5</th>
-                        <th>클래스6</th>
-                    </tr>
-                </thead>
-                <tbody className="ammo-tbody">
-                    <tr className="ammo-tr">
-                        <td>
-                            <img src={ammoImg} alt="ammoImg" />
-                        </td>
-                        <td>m855a1</td>
-                        <td>49</td>
-                        <td style={{ backgroundColor: getRecoilStyle(5), color: 'white' }}>5</td>
-                        <td style={{ backgroundColor: getAccuracyStyle(-10), color: 'white' }}>-10%</td>
-                        <td style={{ backgroundColor: getPenetrateStyle(6) }} className="class1">
-                            6
-                        </td>
-                        <td style={{ backgroundColor: getPenetrateStyle(6) }} className="class2">
-                            6
-                        </td>
-                        <td style={{ backgroundColor: getPenetrateStyle(6) }} className="class3">
-                            6
-                        </td>
-                        <td style={{ backgroundColor: getPenetrateStyle(6) }} className="class4">
-                            6
-                        </td>
-                        <td style={{ backgroundColor: getPenetrateStyle(5) }} className="class5">
-                            5
-                        </td>
-                        <td style={{ backgroundColor: getPenetrateStyle(4) }} className="class6">
-                            4
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div>
+            <div className="ammo-text-container">
+                <div>
+                    <h2>밑의 숫자는 관통수치를 간단하게 나타낸것(참고만 할 것)</h2>
+                    <h2> 1 - 쏘는게 의미 없음</h2>
+                    <h2> 2 - 아주 희박한 확률로 관통</h2>
+                    <h2> 3 - 운좋으면 가끔 관통함 </h2>
+                    <h2>4 - 여전히 관통률이 낮지만 방어구를 빨리 부술 수 있음</h2>
+                    <h2> 5 - 최대 90% 확률로 관통함 </h2>
+                    <h2>6 - 초탄부터 80% 확률, 거의 모든 탄이 관통함</h2>
+                </div>
+            </div>
+            <div className="container">
+                <table className="ammo-table">
+                    <thead className="ammo-thead">
+                        <tr className="ammo-thead-tr">
+                            <th>탄약 사진</th>
+                            <th>탄약 종류</th>
+                            <th>탄약 이름</th>
+                            <th>탄약 데미지</th>
+                            <th>반동</th>
+                            <th>정밀도</th>
+                            <th>클래스1</th>
+                            <th>클래스2</th>
+                            <th>클래스3</th>
+                            <th>클래스4</th>
+                            <th>클래스5</th>
+                            <th>클래스6</th>
+                        </tr>
+                    </thead>
+                    <tbody className="ammo-tbody">
+                        {ammo.map((data) => (
+                            <tr className="market-tbody-tr">
+                                <th>
+                                    <img src="#" alt={data.name} />
+                                </th>
+                                <td>{data.id}</td>
+                                <td>{data.name}</td>
+                                <td>{data.damage}</td>
+                                <td style={getRecoilStyle(data.speed)}>{data.speed}</td>
+                                <td style={getAccuracyStyle(data.accuracy)}>{data.accuracy}</td>
+                                <td style={getPenetrateStyle(data.class1)}>{data.class1}</td>
+                                <td style={getPenetrateStyle(data.class2)}>{data.class2}</td>
+                                <td style={getPenetrateStyle(data.class3)}>{data.class3}</td>
+                                <td style={getPenetrateStyle(data.class4)}>{data.class4}</td>
+                                <td style={getPenetrateStyle(data.class5)}>{data.class5}</td>
+                                <td style={getPenetrateStyle(data.class6)}>{data.class6}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
