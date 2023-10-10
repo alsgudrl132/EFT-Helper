4a9c4a58e828419cc40d95696e3f06336fa781a1
alsgudrl132 <alsgudrl132@gmail.com>
2023-10-10 16:50:54 +0900

    1

--- eft-helper/src/components/headwear/Headwear.js
+++ eft-helper/src/components/headwear/Headwear.js
@@ -1,17 +1,18 @@
-import mainlogo from "../../images/logo2.png";
-import Nav from "../Nav";
-import HeadwearData from "./HeadwearData";
+import mainlogo from '../../images/logo2.png';
+import Nav from '../Nav';
+import HeadwearData from './HeadwearData';
+let i = 0;
 
 const Headwear = () => {
-  return (
-    <div className="background">
-      <div className="container">
-        <img src={mainlogo} alt="logo" />
-        <h1>Helper</h1>
-      </div>
-      <Nav />
-      <HeadwearData />
-    </div>
-  );
+    return (
+        <div className="background">
+            <div className="container">
+                <img src={mainlogo} alt="logo" />
+                <h1>Helper</h1>
+            </div>
+            <Nav />
+            <HeadwearData />
+        </div>
+    );
 };
 export default Headwear;
