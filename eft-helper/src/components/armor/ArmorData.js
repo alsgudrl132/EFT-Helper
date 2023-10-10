b0e382a6ce3de037faba9492581ee748afe7a901
alsgudrl132 <alsgudrl132@gmail.com>
2023-10-10 16:34:07 +0900

    방탄페이지 이미지추가

--- eft-helper/src/components/armor/ArmorData.js
+++ eft-helper/src/components/armor/ArmorData.js
@@ -1,67 +1,73 @@
-import { useEffect, useState } from "react";
-import armorTest from "../../images/3M_icon.webp";
-import "./ArmorData.css";
+import { useEffect, useState } from 'react';
+import './ArmorData.css';
+const images = [];
+for (let i = 1; i <= 30; i++) {
+    const img = require(`./img/${i}.webp`);
+    images.push(img);
+}
 
 const ArmorData = ({ searchValue }) => {
-  const [armor, setArmor] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://125.142.47.191:5000/EFT_API/ARMOR_ALL_SELECT");
-        const data = await response.json();
-        console.log(data);
-        setArmor(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+    const [armor, setArmor] = useState([]);
 
-    fetchData();
-  }, []);
-  return (
-    <div>
-      <div className="armor-text-container">
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://125.142.47.191:5000/EFT_API/ARMOR_ALL_SELECT');
+                const data = await response.json();
+                setArmor(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
         <div>
-          <h2>방탄 정보</h2>
+            <div className="armor-text-container">
+                <div>
+                    <h2>방탄 정보</h2>
+                </div>
+            </div>
+            <div className="armor-container">
+                <table className="armor-table">
+                    <thead className="armor-thead">
+                        <tr className="armor-thead-tr">
+                            <th>방탄 이미지</th>
+                            <th>이름</th>
+                            <th>방탄 클래스</th>
+                            <th>보호부위</th>
+                            <th>재질</th>
+                            <th>내구도</th>
+                            <th>이동속도</th>
+                            <th>회전속도</th>
+                            <th>타입</th>
+                            <th>무게</th>
+                        </tr>
+                    </thead>
+                    <tbody className="armor-tbody">
+                        {armor.map((data, index) => (
+                            <tr className="armor-tbody-tr" key={index}>
+                                <td>
+                                    <img className="armor-img" src={images[index]} alt="img" />
+                                </td>
+                                <td>{data.name}</td>
+                                <td>{data.class}</td>
+                                <td>{data.areas}</td>
+                                <td>{data.material}</td>
+                                <td>{data.durability}</td>
+                                <td>{data.movement_speed}</td>
+                                <td>{data.turning_speed}</td>
+                                <td>{data.type}</td>
+                                <td>{data.weight}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
-      <div className="armor-container">
-        <table className="armor-table">
-          <thead className="armor-thead">
-            <tr className="armor-thead-tr">
-              <th>방탄 이미지</th>
-              <th>이름</th>
-              <th>방탄 클래스</th>
-              <th>보호부위</th>
-              <th>재질</th>
-              <th>내구도</th>
-              <th>이동속도</th>
-              <th>회전속도</th>
-              <th>타입</th>
-              <th>무게</th>
-            </tr>
-          </thead>
-          <tbody className="armor-tbody">
-            {armor.map((data, index) => (
-              <tr key={index} className="armor-tbody-tr">
-                <th>
-                  <img className="armor-img" src={armorTest} alt="3M Icon" />
-                </th>
-                <td>{data.name}</td>
-                <td>{data.class}</td>
-                <td>{data.areas}</td>
-                <td>{data.material}</td>
-                <td>{data.durability}</td>
-                <td>{data.movement_speed}</td>
-                <td>{data.turning_speed}</td>
-                <td>{data.type}</td>
-                <td>{data.weight}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    );
 };
+
 export default ArmorData;
